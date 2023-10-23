40a1c4fb16ab5ea5b81dd8479e8e630c4976bf3c
WhaleFell <whalefall9420@outlook.com>
2023-10-23 10:06:00 +0800

    debug

--- api/table.js
+++ api/table.js
@@ -542,7 +542,7 @@ route.get('/table/bet/search', (req, res) => {
         } else if (common.oc(req)) {
             // SELECT * FROM bet where telegramid = "74628270";
             connection.query(`SELECT * FROM bet where ${params.key} = "${params.value}";`, (error, result) => {
-                console.log(`query error:${err}`)
+                console.log(`query error:${error}`)
                 connection.destroy();
                 if (!result) {
                     common.reqError(res);
